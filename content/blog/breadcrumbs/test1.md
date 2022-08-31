fe0e6716eb86126968235c1dacf7d253998622a5
ryuna <anottrx@gmail.com>
2022-08-31 16:50:45 +0900

    init

---
+++ content/blog/breadcrumbs/test1.md
@@ -0,0 +1,8 @@
+---
+title: '빵부스러기'
+date: 2020-07-02 16:21:13
+category: 'breadcrumbs'
+draft: true
+---
+
+Test1
---
+++ src/pages/about.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import { Layout } from '../layout'
+import { rhythm } from '../utils/typography'
+import * as Lang from '../constants'
+
+export default ({ data }) => {
+  const resumes = data.allMarkdownRemark.edges
+
+  const resume = resumes
+    .filter(({ node }) => node.frontmatter.lang === Lang.ENGLISH)
+    .map(({ node }) => node)[0]
+
+  return (
+    <Layout location={'/'} title={'record'}>
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(28),
+          padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
+            3 / 4
+          )}`,
+        }}
+      >
+        <div dangerouslySetInnerHTML={{ __html: resume.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(filter: { frontmatter: { category: { eq: null } } }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 160)
+          html
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            lang
+          }
+        }
+      }
+    }
+  }
+`

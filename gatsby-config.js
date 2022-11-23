f7b867aeed213b0c706d4cf0ab1f6360bdd9375c
ryuna <anottrx@gmail.com>
2022-11-23 20:00:57 +0900

    chore: meta-config 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,8 +96,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://anottrx.github.io',
+        sitemap: 'https://anottrx.github.io/sitemap.xml',
         policy: [
           {
             userAgent: '*',

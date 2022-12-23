d86343ab08d98a113094b3adbc0668a50a8de1e5
ryuna <anottrx@gmail.com>
2022-12-23 23:45:28 +0900

    feat: 포스트 상단 nav 추가

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'gatsby'
 import { GitHubIcon } from '../social-share/github-icon'
 
 import './index.scss'
 
 export const Top = ({ title, location, rootPath }) => {
-  const isRoot = location.pathname === rootPath
+  const isRoot = location.pathname === rootPath;
+
+	const onScroll = () => {
+    const height = document.body.scrollHeight - innerHeight;
+    document.querySelector("nav").style.width = (window.pageYOffset / height * 100) + "%";
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", onScroll);
+		return () => window.removeEventListener("scroll", onScroll);
+	}, []);
+
+  
   return (
     <div className="top">
+      <nav className="navbar" />
       {!isRoot && (
         <Link to={`/`} className="link">
           {title}
